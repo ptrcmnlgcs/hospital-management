be4784b4ee336e5ee2ef8989d9beb131846cca80
ptrcmnlgcs <gecosopatricia32@.gmail.com>
2024-06-10 17:39:09 +0800

    Add middleware

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,16 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout']);
+
+    Route::prefix('admin')->middleware('role:admin')->group(function () {
+
+    });
+
+    Route::prefix('doctor')->middleware('role:doctor')->group(function () {
+
+    });
+
+    Route::prefix('patient')->middleware('role:patient')->group(function () {
+
+    });
 });
